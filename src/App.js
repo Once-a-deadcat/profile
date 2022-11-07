244000c17c6c48cebdc940ef2844887dd000132b
catgolem <kugi1255@icloud.com>
2022-11-08 06:15:36 +0900

    [add] my profile

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import * as THREE from 'three'
 import React, { Suspense, useState, useEffect } from 'react'
 import { Canvas, useLoader } from 'react-three-fiber'
 import { useTransition, a } from 'react-spring'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { Model } from './Model'
 import { OrbitControls, draco } from 'drei'
 
-function Model({ url }) {
-  const { nodes, materials } = useLoader(GLTFLoader, url, draco())
-  return (
-    <group rotation={[-Math.PI / 2, 0, 0]} position={[0, -7, 0]} scale={[7, 7, 7]}>
-      <group rotation={[Math.PI / 13.5, -Math.PI / 5.8, Math.PI / 5.6]}>
-        <mesh castShadow receiveShadow geometry={nodes.planet001.geometry} material={materials.scene} />
-        <mesh castShadow receiveShadow geometry={nodes.planet002.geometry} material={materials.scene} />
-      </group>
-    </group>
-  )
-}
+// function Model({ url }) {
+//   const { nodes, materials } = useLoader(GLTFLoader, url, draco())
+//   return (
+//     <group rotation={[-Math.PI / 2, 0, 0]} position={[0, -7, 0]} scale={[7, 7, 7]}>
+//       <group rotation={[Math.PI / 13.5, -Math.PI / 5.8, Math.PI / 5.6]}>
+//         <mesh castShadow receiveShadow geometry={nodes.planet001.geometry} material={materials.scene} />
+//         <mesh castShadow receiveShadow geometry={nodes.planet002.geometry} material={materials.scene} />
+//       </group>
+//     </group>
+//   )
+// }
 
 function Loading() {
   const [finished, set] = useState(false)
@@ -50,26 +51,26 @@ export default function App() {
     <>
       <div className="bg" />
       <h1>
-        LEARN
+        Hello
         <br />
-        <span>w/JASON</span>
+        <span>Good Friends.</span>
       </h1>
-      <Canvas shadowMap camera={{ position: [0, 0, 15] }}>
-        <ambientLight intensity={0.75} />
-        <pointLight intensity={1} position={[-10, -25, -10]} />
+      <Canvas shadowMap camera={{ position: [0, 0, 85] }}>
+        <ambientLight intensity={10} />
+        <pointLight intensity={3.5} position={[0, 180, 100]} />
         <spotLight
           castShadow
-          intensity={2.25}
-          angle={0.2}
+          intensity={12.25}
+          angle={0}
           penumbra={1}
-          position={[25, 25, 25]}
+          position={[25, 50, 25]}
           shadow-mapSize-width={1024}
           shadow-mapSize-height={1024}
           shadow-bias={-0.0001}
         />
-        <fog attach="fog" args={['#cc7b32', 16, 20]} />
+        <fog attach="fog" args={['#d69656', 1, 105]} />
         <Suspense fallback={null}>
-          <Model url="/scene-draco.gltf" />
+          <Model position={[0, -25, 0]} />
         </Suspense>
         <OrbitControls
           autoRotate
@@ -77,15 +78,15 @@ export default function App() {
           enableZoom={false}
           enableDamping
           dampingFactor={0.5}
-          rotateSpeed={1}
+          rotateSpeed={1.5}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
       </Canvas>
       <div className="layer" />
       <Loading />
-      <a href="https://github.com/drcmda/learnwithjason" className="top-left" children="Github" />
-      <a href="https://twitter.com/0xca0a" className="top-right" children="Twitter" />
+      <a href="https://github.com/catgolem" className="top-left" children="Github" />
+      <a href="https://twitter.com/home" className="top-right" children="Twitter" />
       <a href="https://github.com/drcmda/react-three-fiber" className="bottom-left" children="+ react-three-fiber" />
     </>
   )
